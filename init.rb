42ff98420687b5e9da83fbdc5e8e9a2f5fabdda8
Smerth <merth@me.com>
2016-10-14 13:57:18 -0700

    Initial commit Set up the project folder Set up the application paths based on Ruby "$" special variable.

---
+++ init.rb
@@ -0,0 +1,20 @@
+#### Word Finder ####
+#
+# Launch this Ruby file from the command line 
+# to get started.
+#
+
+# This APP_ROOT variable stores the absolute path to this very file.
+# so it is the basis of writing paths to files used in the application.
+APP_ROOT = File.dirname(__FILE__)
+
+# require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
+
+# "$" is a special variable in Ruby.  It contains the paths to directories
+# Ruby is aware of a will look in find files it needs
+# .unshift method appends to the beginning of this list of paths the path
+# to a new directory we want Ruby to be aware of
+$:.unshift( File.join(APP_ROOT, 'lib') )
+require 'guide'
